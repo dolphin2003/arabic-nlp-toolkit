f078200f77961e1466a267795d865ae0d104fa46
dolphin2003 <dolphin2003@proton.me>
2023-10-02 16:17:24 +0000

    sched: Update cpu load after task_tick

---
+++ anltk/include/anltk/char_maps.h
@@ -0,0 +1,39 @@
+#ifndef CHAR_MAPS_H
+#define CHAR_MAPS_H
+
+#include "anltk/anltk_typedefs.h"
+#include <array>
+#include <map>
+namespace anltk
+{
+// clang-format off
+// alphapet
+constexpr char_t HAMZA            = U'\u0621'; // ء	 \u0621
+constexpr char_t ALEF_MADDA       = U'\u0622'; // آ	 \u0622
+constexpr char_t ALEF_HAMZA_ABOVE = U'\u0623'; // أ	 \u0623
+constexpr char_t WAW_HAMZA_ABOVE  = U'\u0624'; // ؤ	 \u0624
+constexpr char_t ALEF_HAMZA_BELOW = U'\u0625'; // إ	 \u0625
+constexpr char_t YEH_HAMZA_ABOVE  = U'\u0626'; // ئ	 \u0626
+constexpr char_t ALEF_NO_HAMZA    = U'\u0627'; // ا	 \u0627
+constexpr char_t BEH              = U'\u0628'; // ب	 \u0628
+constexpr char_t TEH_MARBOOTA     = U'\u0629'; // ة	 \u0629
+constexpr char_t TEH              = U'\u062A'; // ت	 \u062A
+constexpr char_t THEH             = U'\u062B'; // ث	 \u062B
+constexpr char_t JEEM             = U'\u062C'; // ج	 \u062C
+constexpr char_t HAH              = U'\u062D'; // ح	 \u062D
+constexpr char_t KHAH             = U'\u062E'; // خ	 \u062E
+constexpr char_t DAL              = U'\u062F'; // د	 \u062F
+constexpr char_t THAL             = U'\u0630'; // ذ	 \u0630
+constexpr char_t REH              = U'\u0631'; // ر	 \u0631
+constexpr char_t ZAIN             = U'\u0632'; // ز	 \u0632
+constexpr char_t SEEN             = U'\u0633'; // س	 \u0633
+constexpr char_t SHEEN            = U'\u0634'; // ش	 \u0634
+constexpr char_t SAD              = U'\u0635'; // ص	 \u0635
+constexpr char_t DAD              = U'\u0636'; // ض	 \u0636
+constexpr char_t TAH              = U'\u0637'; // ط	 \u0637
+constexpr char_t ZAH              = U'\u0638'; // ظ	 \u0638
+constexpr char_t AIN              = U'\u0639'; // ع	 \u0639
+constexpr char_t GHAIN            = U'\u063A'; // غ	 \u063A
+constexpr char_t FEH              = U'\u0641'; // ف	 \u0641
+constexpr char_t QAF              = U'\u0642'; // ق	 \u0642
+constexpr char
