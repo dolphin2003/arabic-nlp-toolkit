5e4dd91254d5130fd366b7701bb8b99a9995cf81
dolphin2003 <dolphin2003@proton.me>
2024-01-16 19:14:15 +0000

    Update cs.ini

--- anltk/src/tafqit.cpp
+++ anltk/src/tafqit.cpp
@@ -9,4 +9,31 @@
 * @author        : Mohsen Alyafei
 * @date          : 04 August 2020
 * @Licence       : MIT
-* @param         : {Number} [Integer in Numeric or String 
+* @param         : {Number} [Integer in Numeric or String form]
+*                  Number may be in Arabic-Indic format (as a string)
+* @param         : [{options}] 9 Options passed as object {name:value} as follows:
+*
+* {Feminine}     : "on": Generate string for a Feminine subject (أرقام بصيغة المؤنث).
+*                        The default is the Masculine form.
+* {Miah}         : "on": Use Mi'ah for Hundreds (مئة بدل مائة). Default is Ma'ah "مائة".
+* {Comma}        : "on": Insert comma between triplet words.
+* {SplitHund}    : "on": Split number from hundred words (فصل الرقم عن المئة).
+                   i.e. ثلاث مائة. Default "No Split" i.e. (ثلاثمائة).
+* {Billions}     : "on": Use Billions (بليون) instead of Miliard (مليار).
+* {TextToFollow} : "on": Indicates that there will be text to follow the resulting text.
+*                  This permits proper subject name to be added after the resulting text.
+* {AG}           : "on": Text is produced in Accusative/Genitive (جر/نصب) case.
+*                  The default is the Nominative cse (رفع).
+*
+* {Subjec}       : An optional array holding the Subject Name to be counted in 4 forms as follows:
+*                  [0] = Deafult Name Singular      (e.g. "كتاب/تفاحة/دينار").
+*                  [1] = Name for 2's (double)      (e.g. "كتابان/تفاحتان/ديناران").
+*                  [2] = Name for plural            (e.g. "كتب/تفاحات/دنانير").
+*                  [3] = Name Singular with Tanween (e.g. "كتابًا/تفاحةً/دينارًا").
+*                  The subject name will be added to the resulting string in acordance
+*                  with the number grammar rules.
+* {Legal}        : "on" Uses the lagal form of output text.
+*
+* @returns       : {string} The wordified number string in Arabic.
+**********************************************************************/
+#include 
