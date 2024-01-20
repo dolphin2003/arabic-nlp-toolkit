9506ccde44f4b2d970e70db81fd7f6613cc5193a
dolphin2003 <dolphin2003@proton.me>
2024-01-20 07:53:01 +0000

    Normalize projects.yaml

--- anltk/src/tafqit.cpp
+++ anltk/src/tafqit.cpp
@@ -79,4 +79,38 @@ std::string tafqit(long long Num, TafqitOptions opts)
 	string Taa = IsAG ? "تي" : "تا";
 	string Taan = IsAG ? "تين" : "تان"; // Hundred 2's مئتا/مائتا مئتان/مائتان
 	string Aa = IsAG ? "ي" : "ا";
-	string Aan = IsAG ? "ين" : "ان"; // Scale 2's ا
+	string Aan = IsAG ? "ين" : "ان"; // Scale 2's الفا/مليونا الفان/مليونان
+	string Ethna   = IsAG ? "اثني" : "اثنا";
+	string Ethnata = IsAG ? "اثنتي" : "اثنتا"; // Masculine/Feminine 12 starting word
+	string Ethnan  = IsAG ? "اثنين" : "اثنان";
+	string Ethnatan = IsAG ? "اثنتين" : "اثنتان"; // Masculine/Feminine 2
+	string Woon     = IsAG ? "ين" : "ون"; // Second part of 20's to 90's
+	bool IsSubject       = opts.subjects.size() == 4; // Check for Subject Array Names
+
+	opts.has_followup_text = opts.has_followup_text == ON; // TextToFollow Option Flag
+	if (IsSubject)
+		opts.has_followup_text = false; // Disable TextToFollow Option if Subject Option is ON
+	string NumIn = std::to_string(Num); // Make numeric string
+	// NumIn = "" + NumIn.replace(/[٠-٩]/g, d => "٠١٢٣٤٥٦٧٨٩".indexOf(d)); // Convert Arabic-Indic
+	// Numbers to Arabic if any
+	string MiahStr = (opts.use_miah == ON) ? "مئة" : "مائة"; // Select chosen Miah (Hundred) Option
+
+	vector<string> TableUnits = TableMale;
+	vector<string> Table11_19
+	    = TableMale; // Create copies of Masculine Table for manipulation
+	Table11_19[0] = TableFemale[10]; // Borrow word "عشرة" from Feminine's Table for use in 11-19
+	Table11_19[1] = Ahad; // Masculine starting words for 11
+	Table11_19[2] = Ethna; // Masculine starting words for 12
+	TableUnits[2] = Ethnan; // Masculine word for 2
+
+	//------------------------------------------------------------------
+	//    Core Function Converts 1 Triplet (1 to 999) to Arabic Words
+	//------------------------------------------------------------------
+	auto oneTripletToWords = [&]()
+	{
+		Num_99               = Triplet % 100; // 00 to 99
+		long long Num_100    = (Triplet / 100); // Hundreds (1 digit)
+		long long Num_Unit   = Num_99 % 10; // 0 to 9 (1 digit)
+		long long Num_Tens   = (Num_99 / 10); // Tens   (1 digit)
+		string Word_100 = "";
+		strin
